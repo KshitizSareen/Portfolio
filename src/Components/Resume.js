aa0729fd794326b9c4652e63e000d8d391da0098
KshitizSareen <kshitizsareen709@gmail.com>
2022-09-18 02:40:22 -0700

    Made it look better

---
+++ src/Components/Resume.js
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+import Slide from "react-reveal";
+
+class Resume extends Component {
+  getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  render() {
+    if (!this.props.data) return null;
+
+    const skillmessage = this.props.data.skillmessage;
+    const education = this.props.data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+        </div>
+      );
+    });
+
+    const work = this.props.data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>{work.description1}</p>
+          <p>{work.description2}</p>
+          <p>{work.description3}</p>
+          <p>{work.description4}</p>
+        </div>
+      );
+    });
+
+    const programmingskills = this.props.data.ProgrammingSkills.map((skills) => {
+      const backgroundColor = this.getRandomColor();
+      const className = "bar-expand " + skills.name.toLowerCase();
+      const width = skills.level;
+
+      return (
+        <li key={skills.name}>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+    
+    const tools = this.props.data.tools.map((skills) => {
+      const backgroundColor = this.getRandomColor();
+      const className = "bar-expand " + skills.name.toLowerCase();
+      const width = skills.level;
+
+      return (
+        <li key={skills.name}>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+
+    const operatingSystems = this.props.data.systems.map((skills) => {
+      const backgroundColor = this.getRandomColor();
+      const className = "bar-expand " + skills.name.toLowerCase();
+      const width = skills.level;
+
+      return (
+        <li key={skills.name}>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+
+    return (
+      <section id="resume">
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Education</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{education}</div>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row work">
+            <div className="three columns header-col">
+              <h1>
+                <span>Work</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">{work}</div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+        <div className="row skill">
+            <div className="three columns header-col">
+              <h1>
+                <span>Skills</span>
+              </h1>
+            </div>
+        <div className="nine columns main-col">
+              <h3>Programming Languages</h3>
+                <ul className="skills">{programmingskills}</ul>
+            
+            <h3>Frameworks, Tools and Technologies</h3>
+                <ul className="skills">{tools}</ul>
+
+            <h3>Operating Systems</h3>
+                <ul className="skills">{operatingSystems}</ul>
+                
+            </div>
+            </div>
+        </Slide>
+      </section>
+    );
+  }
+}
+
+export default Resume;
